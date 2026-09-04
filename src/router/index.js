86a0db91c4e767f7f6e22088b22a667ce414e9c5
maikezzk <maikezzk7@gmail.com>
2026-09-03 22:35:42 -0300

    Turquia

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// Paginas Públicas
+// Páginas Públicas
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 
 // Layout do Painel
 import AppLayout from '../layouts/AppLayout.vue'
 
-//Páginas Internas
+// Páginas Internas
 import UploadView from '../views/UploadView.vue'
 import RelatoriosView from '../views/RelatoriosView.vue'
 import GraficosView from '../views/GraficosView.vue'
 
 const routes = [
-    //Páginas inicial
-    {
-        path:'/',
-        name:'home',
-        component: HomeView
-    },
+  // Página inicial
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
 
-    //Login
-    {
-        path: '/login',
-        name: 'login',
-        component: LoginView
-    },
+  // Login
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
 
-    // Painel interno
-    {
-        path: '/app',
-        component: AppLayout,
-        redirect:'/app/uploud',
+  // Painel interno
+  {
+    path: '/app',
+    component: AppLayout,
+    redirect: '/app/upload',
 
-        Children:[
-            {
-                path: 'upload',
-                name: 'upload',
-                component:UploadView
-            },
+    children: [
+      {
+        path: 'upload',
+        name: 'upload',
+        component: UploadView
+      },
 
-            {
-                path: 'relatorios',
-                name: 'relatorios',
-                component: RelatoriosView
-            },
+      {
+        path: 'relatorios',
+        name: 'relatorios',
+        component: RelatoriosView
+      },
 
-            {
-                path: 'graficos',
-                name: 'graficos',
-                component: GraficosView
-            }
-        ]
-    }
+      {
+        path: 'graficos',
+        name: 'graficos',
+        component: GraficosView
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+  history: createWebHistory(),
+  routes
 })
 
 export default router
